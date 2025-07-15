7f6246848b9079548ef499add1ef59b5d3796a96
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-07-15 15:02:46 +0200

    Initial commit

---
+++ src/main/java/app/cryptarch/CryptarchApplication.java
@@ -0,0 +1,13 @@
+package app.cryptarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/cryptarch/CryptarchApplicationTests.java
@@ -0,0 +1,13 @@
+package app.cryptarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
