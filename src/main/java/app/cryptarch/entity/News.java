b6bf15fadf98d97120ebf89786732f787129ea11
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-07-17 23:42:58 +0200

    Entities have been added

--- src/main/java/app/cryptarch/entity/News.java
+++ src/main/java/app/cryptarch/entity/News.java
@@ -1,11 +1,11 @@
 package app.cryptarch.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @Entity
 @Getter
@@ -14,5 +14,24 @@ public class News {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crypto_id")
+    private Crypto crypto;
+
+    @Column(columnDefinition = "text")
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String content;
+
+    @Column(name = "published_at")
+    private ZonedDateTime publishedAt;
+
+    private String source;
+
+    @Column(columnDefinition = "text")
+    private String url;
 }
